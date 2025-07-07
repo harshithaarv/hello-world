e24c2618607e3bd90f9913034f5b41aa23498032
harshithaarv <harshitha24manjunath@gmail.com>
2025-07-07 13:54:26 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ pipeline{
                 sh 'python3 --version'
             }
         }
-        stage('hello uni'){
+        stage('hello universe'){
             steps{
                 sh 'python3 hello.py'
             }
